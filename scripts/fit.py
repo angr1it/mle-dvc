88c030301ca5bec0543c15bb80035b2483b59f3b
Andrei Gritsenko <andgr1it@gmail.com>
2024-11-04 07:37:33 +0000

    обновление DVC-пайплайна

--- scripts/fit.py
+++ scripts/fit.py
@@ -3,23 +3,23 @@
 from catboost import CatBoostClassifier
 import pandas as pd
 from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
+
 
 import yaml
 import os
 import joblib
 
 
-# обучение модели
 def fit_model():
-    # Прочитайте файл с гиперпараметрами params.yaml
+
     with open("params.yaml", "r") as fd:
         params = yaml.safe_load(fd)
-    # загрузите результат предыдущего шага: inital_data.csv
+
     data = pd.read_csv("data/initial_data.csv")
 
-    # реализуйте основную логику шага с использованием гиперпараметров
     cat_features = data.select_dtypes(include="object")
     potential_binary_features = cat_features.nunique() == 2
     binary_cat_features = cat_features[
@@ -43,8 +43,9 @@ def fit_model():
         verbose_feature_names_out=False,
     )
 
-    model = CatBoostClassifier(auto_class_weights=params["auto_class_weights"])
-
+    # model = CatBoostClassifier(auto_class_weights=params["auto_class_weights"])
+    model = LogisticRegression(penalty=params["penalty"], C=params["regression_c"])
+    
     pipeline = Pipeline([("preprocessor", preprocessor), ("model", model)])
     pipeline.fit(data, data[params["target_col"]])
     # сохраните обученную модель в models/fitted_model.pkl
